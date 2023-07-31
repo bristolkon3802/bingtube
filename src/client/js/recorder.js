2c9719137ae2cdbd172535ded3fcbaadda0e56b9
bristolkon <bristolkon3802@gmail.com>
2023-07-31 16:08:57 +0900

    비디오 녹화 테스트 1

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,46 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const handleDownload = () => {};
+
+const hendleStop = () => {
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", hendleStop);
+  startBtn.addEventListener("click", handleDownload);
+
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", hendleStop);
+
+  const recorder = new window.MediaRecorder(stream);
+  recorder.ondataavailable = (e) => {
+    //console.log(e.data);
+    const videoFile = URL.createObjectURL(e.data);
+    //console.log(videoFile);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
-  console.log(stream);
+  //console.log(stream);
+  video.srcObject = stream;
+  video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
