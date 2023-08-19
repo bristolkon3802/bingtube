850b180ed01e99601668341c45dbdc3ab4e29d36
bristolkon <bristolkon3802@gmail.com>
2023-08-19 16:05:59 +0900

    port 변경

--- src/init.js
+++ src/init.js
@@ -5,7 +5,7 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = 8000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => {
   console.log(`Server listenting 포트: http://localhost:${PORT}`);
